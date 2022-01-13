a03a862679c22f3196c8a47dac04c6241c80c4eb
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-01-13 20:40:41 +0530

    #include <stdio.h> #include <math.h>
    int main()
    {
        int num;
        int iVar;
        float fVar;
      
        scanf("%d",&num);
      
        fVar=sqrt((double)num);
        iVar=fVar;
     
        if(iVar==fVar)
            printf("True");
        else
            printf("False");
          
        return 0;
    }

---
+++ perfect square root or not.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+ 
+int main()
+{
+    int num;
+    int iVar;
+    float fVar;
+  
+    scanf("%d",&num);
+  
+    fVar=sqrt((double)num);
+    iVar=fVar;
+ 
+    if(iVar==fVar)
+        printf("True");
+    else
+        printf("False");
+      
+    return 0;
+}
