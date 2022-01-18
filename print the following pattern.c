f0a90489ce94ed53a8a3931f29be8a8632aa93ce
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-01-18 17:52:30 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             printf("%c ",i+64);         }         printf(" ");     } }

--- print the following pattern.c
+++ print the following pattern.c
@@ -3,11 +3,11 @@ int main()
 {
     int i,j,n;
     scanf("%d",&n);
-    for(i=n;i>=1;i--)
+    for(i=1;i<=n;i++)
     {
-        for(j=n;j>=1;j--)
+        for(j=1;j<=n;j++)
         {
-            printf("%d ",j);
+            printf("%c ",i+64);
         }
         printf("
 ");
