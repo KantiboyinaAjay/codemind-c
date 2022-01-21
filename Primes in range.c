f8b463ef82ff8598f75153aa9cd0b2f46cd7ab92
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-01-21 18:51:22 +0530

    #include<stdio.h> #include<math.h> int main() { 	int i,n,flag,c; 	int l,r;
    	scanf("%d",&l);
    
    	scanf("%d",&r);
    	
    	c = 0; 
    
    	for(n = l;n<=r;++n){
    		
    
    		if(n==1)
    			continue;
    		
    		
    		flag = 0;
    		for(i = 2;i<=sqrt(n);++i){
    			
    			if(n%i==0){
    				flag=1;
    				break;
    			}
    		}
    		
    		if(flag==0)
    			++c;
    			
    	}
    	
    	printf("%d",c);
    	
    }

---
+++ Primes in range.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<math.h>
+int main() {
+	int i,n,flag,c;
+	int l,r;
+	
+
+	scanf("%d",&l);
+
+	scanf("%d",&r);
+	
+	c = 0; 
+
+	for(n = l;n<=r;++n){
+		
+
+		if(n==1)
+			continue;
+		
+		
+		flag = 0;
+		for(i = 2;i<=sqrt(n);++i){
+			
+			if(n%i==0){
+				flag=1;
+				break;
+			}
+		}
+		
+		if(flag==0)
+			++c;
+			
+	}
+	
+	printf("%d",c);
+	
+}
