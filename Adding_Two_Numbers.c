1bc379ac6883b3b09ae9232965a4d1194f833ecf
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-01-28 21:31:30 +0530

    #include<stdio.h> int main()     {         int num1, num2, sum;
            scanf("%d", &num1);
            
            scanf("%d", &num2);
            
            sum = num1 + num2;
            printf("%d", sum);
            return 0;
        }

---
+++ Adding_Two_Numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+    {
+        int num1, num2, sum;
+        
+        scanf("%d", &num1);
+        
+        scanf("%d", &num2);
+        
+        sum = num1 + num2;
+        printf("%d", sum);
+        return 0;
+    }
