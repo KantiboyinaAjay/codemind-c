5c611636ff34678c59511e8abeb02cc65c24f614
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-04-24 15:52:07 +0530

    #include<stdio.h> int main() {     int n,m,sum=0,mul=1;     scanf("%d",&n);     for(int i=0;n>0;i++)     {         m=n%10;         n=n/10;         sum+=m;         mul*=m;     }     if(sum==mul)     {         printf("Spy Number");     }     else     {         printf("Not Spy Number");     } }

--- Spy_Number.c
+++ Spy_Number.c
@@ -1,22 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    int num,s=0,m=1,temp,d;
-    scanf("%d",&num);
-    temp=num;
-    while(temp!=0)
+    int n,m,sum=0,mul=1;
+    scanf("%d",&n);
+    for(int i=0;n>0;i++)
     {
-        d=temp%10;
-        s=s+d;
-        m=m*d;
-        temp=temp/10;
+        m=n%10;
+        n=n/10;
+        sum+=m;
+        mul*=m;
     }
-    
-    
-    if(s==m)
-    printf("Spy Number");
+    if(sum==mul)
+    {
+        printf("Spy Number");
+    }
     else
-    printf("Not Spy Number");
-    return 0;
-    
+    {
+        printf("Not Spy Number");
+    }
 }
