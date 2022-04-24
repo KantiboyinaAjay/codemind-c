12fff99f090eead5d652043e0be5f441a3e97d5d
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-04-24 16:49:23 +0530

    #include<stdio.h> int main() {     int i,a,c,b;     scanf("%d %d",&a,&b);     for(i=1;i<=b;i++)     {         c = i * a;         if(c%b==0)         {             printf("%d",c);             break;         }     } }

--- LCM.c
+++ LCM.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
 int main()
 {
-	int a,b,c,i;
-	scanf("%d %d",&a,&b);
-	for(i=1;i<=b;i++)
-	{
-		c=i*a;
-		if(c%b==0)
-	    {
-	    	printf("%d ",c);
-	    	break;
-	    }
-	}
-	return 0;
+    int i,a,c,b;
+    scanf("%d %d",&a,&b);
+    for(i=1;i<=b;i++)
+    {
+        c = i * a;
+        if(c%b==0)
+        {
+            printf("%d",c);
+            break;
+        }
+    }
 }
