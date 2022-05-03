d9b6fc781489a0ba61c4e406fd8d6fef2a2c3888
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-05-03 16:58:36 +0530

    #include<stdio.h> int main() { 	int i,n,m=0; 	int arr[n]; 	scanf("%d",&n); 	for(i=0;i<n;i++) 	{ 		scanf("%d",&arr[i]); 	} 	for(i=0;i<n;i++) 	{ 		if(i%2!=0) 		{ 			m+=arr[i]; 		} 	} 	printf("%d",m); }

---
+++ Sum_of_Odd_Positions.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int i,n,m=0;
+	int arr[n];
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		if(i%2!=0)
+		{
+			m+=arr[i];
+		}
+	}
+	printf("%d",m);
+}
