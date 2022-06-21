5678585f6e02dc5467d61f4a8084cceec73edf2d
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-06-21 21:39:19 +0530

    #include<stdio.h> int main() {     int n,i,j,c=0;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         for(j=0;j<n;j++)         {             if(arr[i]==arr[j])             {                 c++;             }         }         if(c==1)             {                 printf("%d",arr[i]);             }         else             c=0;     } }

---
+++ Single_Number.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,c=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            {
+                c++;
+            }
+        }
+        if(c==1)
+            {
+                printf("%d",arr[i]);
+            }
+        else
+            c=0;
+    }
+}
