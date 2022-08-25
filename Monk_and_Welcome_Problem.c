e90ce277c4641d77c80d1e5f98b629827f42840f
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-08-25 14:32:27 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],b[n];     for(int i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(int i=0;i<n;i++)     {         scanf("%d",&b[i]);     }     for(int i=0;i<n;i++)     {         for(int j=0;j<n;j++)         {             if(i==j)             {                 printf("%d ",a[i]+b[j]);             }         }     } }

---
+++ Monk_and_Welcome_Problem.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],b[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&b[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(i==j)
+            {
+                printf("%d ",a[i]+b[j]);
+            }
+        }
+    }
+}
