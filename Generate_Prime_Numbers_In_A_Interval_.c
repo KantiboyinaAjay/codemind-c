84dfc3c99cd196f199fbaa739b078c1127e38229
KantiboyinaAjay <96778114+KantiboyinaAjay@users.noreply.github.com>
2022-10-15 22:10:54 +0530

    #include<stdio.h> #include<math.h> int prime(int n) {     if(n==1)     {         return 0;     }     if(n==2)     {         return 1;     }     for(int i=2;i<sqrt(n)+1;i++)     {         if(n%i==0)         {             return 0;         }     }     return 1; } int main() {     int t,m;     scanf("%d %d",&t,&m);     for(int i=t;i<=m;i++)     {         if(prime(i)==1)         {             printf("%d ",i);         }     } }

--- Generate_Prime_Numbers_In_A_Interval_.c
+++ Generate_Prime_Numbers_In_A_Interval_.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 #include<math.h>
-int is_prime(int n)
+int prime(int n)
 {
     if(n==1)
     {
         return 0;
     }
     if(n==2)
+    {
         return 1;
+    }
     for(int i=2;i<sqrt(n)+1;i++)
     {
         if(n%i==0)
@@ -19,13 +21,13 @@ int is_prime(int n)
 }
 int main()
 {
-    int a,b;
-    scanf("%d %d",&a,&b);
-    for(int i=a;i<=b;i++)
+    int t,m;
+    scanf("%d %d",&t,&m);
+    for(int i=t;i<=m;i++)
     {
-        if(is_prime(i)==1)
+        if(prime(i)==1)
         {
-             printf("%d
+            printf("%d
 ",i);
         }
     }
